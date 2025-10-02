090f57f1b51633167bbd702ff2f826ba50223184
riffraff4242 <arifolumo235@gmail.com>
2025-10-02 13:27:59 +0100

    Add files via upload

---
+++ scripy2.js
@@ -0,0 +1,38 @@
+let david;
+
+console.log(david)
+david = true
+console.log(david)
+david = "person"
+
+console.log(david)
+console.log(david)
+
+david = {
+    type: "person",
+    name: "david",
+    age: 30,
+    isMarried: false,
+    children: ["john", "Jane", " Joe"]
+}
+
+console.log(david)
+let a = 3
+let b = 5
+let x = a + b
+console.log(x)
+
+let d = 4
+let e = 5
+let y = d**e
+console.log(y)
+
+let f = 2 
+let g = 6
+let z = g - f
+console.log(z)
+
+let h = 8
+let i = 16
+let q = i / h
+console.log(q)
